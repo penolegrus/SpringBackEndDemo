64343e62fe0586b74b59fe3005e6563b13ecf1b4
Oleg Pendrak <38357504+penolegrus@users.noreply.github.com>
2022-10-20 16:00:32 +0500

    Delete scripts directory

--- scripts/runUI.sh
+++
@@ -1,6 +0,0 @@
-ssh -i /Users/o.pendrak/Downloads/SpringBootUbuntuStudy-KGju0M3S.pem ubuntu@85.192.34.140 << EOF
-pgrep node | xargs kill -9
-cd /var/www/threadqa.ru/trainDemo
-live-server --port=8081
-
-EOF
